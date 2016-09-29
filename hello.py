4ea76d3e44d725a264f6eb9764f721cdffb20944
pengqiangnj <pengqiang.gs.nj@gmail.com>
2016-09-29 16:22:09 +0800

    v0.5 commit

--- hello.py
+++ hello.py
@@ -5,11 +5,15 @@
 from flask import Flask
 from flask.ext.script import Manager
 from flask import render_template
+from flask.ext.bootstrap import Bootstrap
 
 application = Flask(__name__)
 
-# init application use manager
+# init application by manager
 manager = Manager(application);
+
+# init application by bootstrap
+bootstrap = Bootstrap(application)
 
 # index view process
 @application.route('/')
@@ -20,6 +24,16 @@ def index():
 @application.route('/<username>')
 def userinfo(username):
     return render_template('user.html', name=username)
+
+# error 404 handler
+@application.errorhandler(404)
+def page_not_found(ex):
+    return render_template('404.html'), 404
+
+# error 500 hander
+@application.errorhandler(500)
+def internal_server_error(ex):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     manager.run()
