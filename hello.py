7bc62cdb6a53fae5c47617266d712a3365be320f
pengqiangnj <pengqiang.gs.nj@gmail.com>
2016-09-29 13:31:03 +0800

    v0.2 commit

--- hello.py
+++ hello.py
@@ -2,4 +2,19 @@
 # _*_ coding: utf-8 _*_
 # Desc: This is a test python program.
 
+from flask import Flask
 
+application = Flask(__name__)
+
+# index view process
+@application.route('/')
+def index():
+    return '<h1>Hello Flask!</h1>'
+
+# view process with parameters
+@application.route('/<username>')
+def userinfo(username):
+    return '<h1>Welcome %s back!</h1>' % username
+
+if __name__ == '__main__':
+    application.run(host='127.0.0.1', port=6688)
